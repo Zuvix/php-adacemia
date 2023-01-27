fa495724e569518fdd31a4cca29edd9d89f75427
Zuvix <samuelkubala@gmail.com>
2023-01-27 15:02:31 +0100

    first task complete

---
+++ 1/index.php
@@ -0,0 +1,58 @@
+<?php
+
+function get_current_time()
+{
+    $now = new DateTime();
+    return $now->format('Y-m-d H:i:s');
+}
+
+function log_arrival_to_file($file, $content, $meskanie)
+{
+    if ($meskanie) {
+        $content = $content . " meskanie";
+    }
+    $handle = fopen($file, 'a');
+    fwrite($handle, $content . "\n");
+    fclose($handle);
+}
+
+function get_logs($file)
+{
+    if (file_exists($file)) {
+        $handle = fopen($file, 'r');
+        $content = fread($handle, filesize($file));
+        echo nl2br($content);
+        fclose($handle);
+    }
+}
+
+//Fake time is for testing purposes
+function log_student($file, $fake_time = false)
+{
+    if ($fake_time) {
+        $time_of_arival = $fake_time;
+    } else {
+        $time_of_arival = get_current_time();
+    }
+
+    $date = strtotime($time_of_arival);
+    $hour = date('H', $date);
+
+    if ($hour >= 20 && $hour < 24) {
+        die("Nemôžem zapísať príchod, lebo je z rozsahu 20-24h.");
+    }
+
+    $meskanie = $hour >= 8 ? true : false;
+    log_arrival_to_file($file, $time_of_arival, $meskanie);
+}
+
+//Main part
+$file = 'log.txt';
+
+//Log real time student arival
+log_student($file);
+
+//Log fake student arival
+log_student($file, '2023-01-27 7:51:32');
+
+get_logs($file);
